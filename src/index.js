9ff532219d36d7d17015f90d12e70207efde9230
etamar <etamar.levy@gmail.com>
2021-04-18 13:31:34 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-let addToy = false;
+///-----------------------------------
+///----------add a toy----------------
+///-----------------------------------
 
+let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -12,4 +15,38 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+
+///-----------------------------------
+///------------"GET" toys-------------
+///-----------------------------------
+  const toysMenu = document.getElementById("toy-collection");
+  fetch("http://localhost:3000/toys")
+  .then(resp => resp.json())
+  .then(data => {
+    for (i = 0; i < data.length; i++) {
+      const toy = document.createElement('div');
+      toy.setAttribute("class", "card");
+      const h2 = document.createElement("h2");
+      h2.innerText = `${data[i].name}`
+      const img = document.createElement("img");
+      img.setAttribute("class", "toy-avatar");
+      img.setAttribute("src", `${data[i].image}`);
+      const p = document.createElement('p');
+      p.innerText = `${data[i].likes} Likes`;
+      const likeButton = document.createElement('button');
+      likeButton.setAttribute("class", "like-btn");
+      likeButton.setAttribute('id', `${data[i].id}`);
+      likeButton.innerText = "Like";
+      toysMenu.append(toy);
+      toy.append(h2, img, p, likeButton);
+    }
+  })
 });
+
+
+
+
+
+
+
